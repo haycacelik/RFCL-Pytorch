Seems to run reverse curriculum. However maybe a bug in step calculation for training

rfclTorch/agents/sac/networks.py
@@ -10,6 +10,7 @@ import copy
 import numpy as np
 import torch
 import torch.nn as nn
+import math
 # import flax
 # import flax.linen as nn
 # import jax
@@ -66,7 +67,7 @@ class Ensemble(nn.Module):
         return torch.stack(out,dim=0)
     
     def sample(self,numSample:int):
-        return random.sample(self.ensemble,numSample)
+        return random.sample(list(self.ensemble),numSample)
     
         
 
@@ -82,8 +83,6 @@ class Critic(nn.Module):
 
     #maybe have np array as input and to tensor()?
     def forward(self, obs: torch.Tensor, acts: torch.Tensor) -> torch.Tensor:
-        obs = torch.tensor(obs,dtype=torch.float32)
-        acts = torch.tensor(acts,dtype=torch.float32)
         x = torch.cat([obs, acts],dim= -1)
         features = self.feature_extractor(x)
         value = self.fc(features)
@@ -211,8 +210,8 @@ class ActorCritic(nn.Module):
         self.to(self.device)
 
     def act(self, obs, deterministic=False):
-        
-        obs = torch.tensor(obs,device=self.device,dtype=torch.float32)
+        if not isinstance(obs,torch.Tensor):
+            obs = torch.tensor(obs,device=self.device,dtype=torch.float32)
         """Sample actions deterministicly"""
         
         return self.actor(obs, deterministic=deterministic)
@@ -256,14 +255,18 @@ class ActorCritic(nn.Module):
         
         nextQ,_ = torch.min(nextQs,dim=0)
         
-        targetQ = batch.reward + discount * batch_mask * next_q
+        targetQ = batch_reward + discount * batch_mask * nextQ
         
 
         if backup_entropy:
             targetQ -= discount * batch_mask * self.temp() * next_log_probs
+            
+        
           
         Qs = self.critic(batch_next_obs,next_actions)  
 
+        targetQ = targetQ.unsqueeze(0).expand_as(Qs)
+        
         loss = self.criticLoss(Qs,targetQ)#might have issues with shape here!
         loss.backward()
         self.critic_optim.step()
@@ -271,14 +274,15 @@ class ActorCritic(nn.Module):
 
     def updateActor(self,batch: TimeStep):
         
+        batch_env_obs = torch.tensor(batch.env_obs,device=self.device,dtype=torch.float32)
         self.actor_optim.zero_grad()
-        dist = self.actor(batch.env_obs)
+        dist = self.act(batch_env_obs)
         actions = dist.sample()
         log_probs = dist.log_prob(actions)
-        Qs = self.critic(batch.env_obs,actions)
+        Qs = self.critic(batch_env_obs,actions)
         Q = torch.mean(Qs,dim=0)
         
-        loss = torch.mean(log_probs * ac.temp() - Q)
+        loss = torch.mean(log_probs * self.temp() - Q)
         loss.backward()
         self.actor_optim.step()
         return -log_probs.mean()

rfclTorch/agents/sac/sac.py
@@ -307,20 +307,18 @@ class SAC(BasePolicy):
                 batch = self.replay_buffer.sample_random_batch( self.cfg.batch_size * self.cfg.grad_updates_per_step)
 
             batch = TimeStep(**batch)
-            ac, update_aux = self.update_parameters(batch)
+            self.update_parameters(batch)
             #shouldn't need this since the sae actor-critic should update it's weights
-            # state = state.replace(
-            #     ac=ac,
-            #     training_steps=training_steps + self.cfg.grad_updates_per_step,
-            # )
+            state.training_steps = training_steps + self.cfg.grad_updates_per_step
+            
             update_time = time.time() - update_time_start
             time_metrics["update_time"] = update_time
 
+        print(f"done training step, time : {update_time}")
         return state, TrainStepMetrics(time=time_metrics, train=train_metrics, update=update_aux, train_stats=train_custom_stats)
 
     def update_parameters(self,batch:TimeStep):
         
-        print(f"update batch: {batch}")
         def split_batch(batch: TimeStep, num_splits: int) -> list[dict[str, np.ndarray]]:
             batch_dict = asdict(batch)
             split_dicts = [
@@ -337,17 +335,15 @@ class SAC(BasePolicy):
         
         #mini_batches = tools.tree_map(lambda x: np.array(np.split(x, update_rounds)), batch)
         mini_batches = split_batch(batch, update_rounds)
-        print(f"mini_batches {mini_batches}")
         for miniBatch in mini_batches:
             
             roundBatches = split_batch(TimeStep(**miniBatch), grad_updates_per_round)
             
             for roundBatch in roundBatches:
-                print(f"round batch: {roundBatch}")
-                self.ActorCritic.updateCritic(roundBatch,self.cfg.discount, self.cfg.backup_entropy, self.cfg.num_min_qs)
+                self.ActorCritic.updateCritic(TimeStep(**roundBatch),self.cfg.discount, self.cfg.backup_entropy, self.cfg.num_min_qs)
                 self.ActorCritic.updateTarget(self.cfg.tau)
-            entropy = self.ActorCritic.updateActor(miniBatch)
-            
+            entropy = self.ActorCritic.updateActor(TimeStep(**miniBatch))
+            entropy = entropy.detach()
             if self.cfg.learnable_temp:
                 self.ActorCritic.updateTemp(entropy,self.cfg.target_entropy)
         